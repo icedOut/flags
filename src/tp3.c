d37e55c24dd6ff4f7dc59fb7c538876cc7c0b2b3
Philippe Chartrand <dapper2474@gmail.com>
2018-08-06 18:01:07 -0400

    MAIN: parsage des noms communs

--- src/tp3.c
+++ src/tp3.c
@@ -10,6 +10,7 @@ int main(int argc, char **argv) {
 	url = "DATA/countries.json";
 	json_t *root;
 	json_error_t error;
+	size_t i;
 
 
 	if(arguments->status != TP3_OK) {
@@ -20,17 +21,46 @@ int main(int argc, char **argv) {
 	root = json_load_file(url, 0, &error);
 
 	if(!root) {
-		 printf( "Erreur de parsage \n");
+		 fprintf(stderr, "Erreur de parsage \n");
+		 return 1;
 	}else{
 		printf("JSON Parsé avec succès \n");
 	}
-if(!json_is_array(root))
-{
-    fprintf(stderr, "error: root is not an array\n");
+	if(!json_is_array(root))
+	{
+    fprintf(stderr, "Erreur de racine\n");
     json_decref(root);
     return 1;
-}
+	}
+
+	for(i = 0; i < json_array_size(root); i++){
+    json_t *data, *nom, *common, *capitale;
+    const char *message_text;
+
+    data = json_array_get(root, i);
+    if(!json_is_object(data)){
+    
+        fprintf(stderr, "Erreur de parsage");
+        json_decref(root);
+        return 1;
+    }
+
+    nom = json_object_get(data,"name");
+    if(!json_is_object(nom)){
+    	fprintf( stderr , "Erreur de nom");
+    	return 1;
+    }
+     common = json_object_get(nom,"common");
+    if(!json_is_string(common)){
+    	fprintf(stderr, "Erreur de nom commun");	
+   	    return 1;
+    }
+    message_text = json_string_value(common);
+    printf("%s\n",message_text);
+
 	}
 	free_arguments(arguments);
 	return 0;
+	}
+
 }
