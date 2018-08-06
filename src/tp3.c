9df9d1113ff8f494395d9f5603aa127699591cbb
Philippe Chartrand <dapper2474@gmail.com>
2018-08-06 17:23:41 -0400

    MAIN: rectification de L'URL du fichier countries.json

--- src/tp3.c
+++ src/tp3.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 
 	struct Arguments *arguments = parse_arguments(argc, argv);
 	char *url;
-	url = "../DATA/countries.json";
+	url = "DATA/countries.json";
 	json_t *root;
 	json_error_t error;
 
@@ -15,15 +15,21 @@ int main(int argc, char **argv) {
 	if(arguments->status != TP3_OK) {
 		return arguments->status;
 	} else {
-		printf("Arguments OK");
+		printf("Arguments OK \n");
 	
-	root = json_load_file( url, 0, &error);
+	root = json_load_file(url, 0, &error);
 
 	if(!root) {
-		 printf( "Erreur de parsage");
+		 printf( "Erreur de parsage \n");
 	}else{
-		printf("JSON Parsé avec succès");
-	}	
+		printf("JSON Parsé avec succès \n");
+	}
+if(!json_is_array(root))
+{
+    fprintf(stderr, "error: root is not an array\n");
+    json_decref(root);
+    return 1;
+}
 	}
 	free_arguments(arguments);
 	return 0;
